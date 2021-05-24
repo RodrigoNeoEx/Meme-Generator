e7a98c5a62c1db1d0264a43ee7c9623d2f1ab1a5
RodrigoNeoEx <draja.acc@gmail.com>
2021-05-24 17:36:44 -0300

    created input to set meme, 'P' to set text on image and countainer for meme

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function createTextInput() {
   createInput.maxLength = 60;
   document.getElementById('meme-image-container').appendChild(createInput);
 }
+
+
+function createTextOnContainer() {
+  const memeText = document.createElement('p');
+  memeText.id = 'meme-text';
+  document.getElementById('meme-image-container').appendChild(memeText);
+}
+
+function createInputImage() {
+  const inputImage = document.createElement('input');
+  inputImage.id = 'meme-insert';
+  inputImage.type = 'file', accept="image/*";
+  document.getElementById('meme-image-container').appendChild(inputImage);
+}
+
+function createImageCountainer() {
+  const imageContainer = document.createElement('div');
+  imageContainer.id = 'meme-image';
+  document.getElementById('meme-image-container').appendChild(imageContainer);
+}
